ba831c8147c3e7fa14eb46f8a745283a6de86bd1
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2025-01-17 00:25:55 +0300

    feat: '02-write-file' done

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const process = require('node:process');
+const readline = require('node:readline');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(pathToFile, {flags: 'w'});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Hi! Enter a text to write into a file or type "exit" to quit.');
+
+rl.on('line', (input) => {
+    if (input === 'exit') {
+        process.exit();
+    } else {
+        writeStream.write(input);
+    }
+})
+
+process.on('SIGINT', () => {
+    process.exit();
+})
+
+process.on('exit', () => {
+    writeStream.end();
+})
